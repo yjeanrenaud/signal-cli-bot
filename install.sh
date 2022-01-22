00f7421e629533eed7cdd75ac819352a513d4c13
Yves Jeanrenaud <yjeanrenaud@users.noreply.github.com>
2022-01-22 21:13:37 +0100

    Create install.sh
    initial version

---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+set -e
+
+echo "Installing yj_signalbot_regular\n"
+sudo cp yj_signalbot_regular.py /usr/local/bin/
+sudo chmod a+x /usr/local/bin/yj_signalbot_regular.py
+
+echo "installing init.d script\n"
+sudo cp yj_signal_bot_init.sh /etc/init.d/
+sudo chmod a+x /etc/init.d/yj_signal_bot_init.sh
+
+echho "starting signalbot in a separate screen\n"
+sudo update-rc.dyj_signal_bot_init.sh defaults
+sudo /etc/init.d/yj_signal_bot_init.sh start
+
+echo "all done\n"
